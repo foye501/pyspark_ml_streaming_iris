9bfe34ecb5a05edc7869670e3ae78e86b96a0a98
Tal Franji <tal.franji@gmail.com>
2018-07-02 09:46:50 +0300

    working version

---
+++ iris2kafka.py
@@ -0,0 +1,57 @@
+# simulate web-logs from server being pushed into a Kafka queue
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+import datetime
+import math
+import random
+import sys
+import time
+
+# running on 34.252.218.48 (internal 172.31.6.169) at April 2017
+# need to install:
+# $sudo pip install kafka-python
+
+iris_csv = "./iris.csv"
+kafka_topic = 'wwwlog'
+kafka_server = '34.242.73.97:9092'
+
+def LoadIris():
+    with open(iris_csv) as f:
+        lines = f.readlines()[1:]  # remove header
+        return lines
+
+
+def Consumer():
+    # Consumer - used for testing the generator
+    consumer = KafkaConsumer('wwwlog', bootstrap_servers=kafka_server)
+    for msg in consumer:
+        print (msg)
+
+
+def RandomLineGenerator():
+    lines = LoadIris()
+    while True:
+        random.shuffle(lines)
+        for line in lines:
+            yield line
+
+def Producer():
+    producer = KafkaProducer(bootstrap_servers=kafka_server)
+    print "PRODUCE..."
+    throttle = 0
+    throttle_n = 10 # count of messages in 100 mesec
+    line_iter = RandomLineGenerator()
+    while True:
+        throttle += 1
+        line = line_iter.next()
+        print line
+        producer.send(kafka_topic, line)
+        if throttle >= throttle_n:
+            throttle = 0
+            time.sleep(0.1)
+
+
+if len(sys.argv) > 1 and sys.argv[1].lower() == "c":
+    Consumer()
+else:
+    Producer()
---
+++ iris_streaming.py
@@ -0,0 +1,95 @@
+import pyspark.ml
+import pyspark.ml.feature
+import pyspark.ml.classification
+import pyspark.sql.functions as funcs
+from pyspark.ml.linalg import Vectors, VectorUDT
+import time
+
+
+# based on https://github.com/Azure/MachineLearningSamples-Iris/blob/master/iris_spark.py
+
+# Get the jar files from:
+# http://central.maven.org/maven2/org/apache/spark/spark-sql-kafka-0-10_2.11/2.1.1/spark-sql-kafka-0-10_2.11-2.1.1.jar
+# http://central.maven.org/maven2/org/apache/kafka/kafka-clients/0.10.2.1/kafka-clients-0.10.2.1.jar
+
+# run this program using
+#$ spark-submit --master="local[*]" --jars spark-sql-kafka-0-10_2.11-2.1.1.jar,kafka-clients-0.10.2.1.jar iris_streaming.py
+def load_iris(spark):
+    iris = spark.read.format("csv"
+         ).option("header", "true"
+         ).option("inferSchema","true"
+         ).load("iris.csv")
+    iris.printSchema()
+    return iris
+
+def create_features_labels(iris):
+    # vectorize all numerical columns into a single feature column
+    feature_cols = iris.columns[:-1]  # all features besides "species" string column
+    assembler = pyspark.ml.feature.VectorAssembler(inputCols=feature_cols, outputCol='features')
+    label_indexer = pyspark.ml.feature.StringIndexer(inputCol='species', outputCol='label')
+    pipe = pyspark.ml.Pipeline(stages=[assembler, label_indexer])
+    pipe_model = pipe.fit(iris)
+    data = pipe_model.transform(iris)
+    return data.select("features", "label")
+
+
+def train_model(data):
+    # use Logistic Regression to train on the training set
+    train, test = data.randomSplit([0.70, 0.30])
+    lr = pyspark.ml.classification.LogisticRegression(regParam=0.01)
+    model = lr.fit(train)
+    return model
+
+
+
+def get_feature_stream(spark):
+    # You need to download Kafka jars when running this notebook.
+    KAFKA = '34.242.73.97:9092'
+    TOPIC = 'wwwlog'
+    iris_raw_stream = spark.readStream.format("kafka")\
+     .option("kafka.bootstrap.servers", KAFKA)\
+     .option("subscribe", TOPIC)\
+     .load()
+
+    # convert Kafka's bin value to string
+    raw1 = iris_raw_stream.selectExpr("CAST(value AS STRING) as val")
+    raw2 = raw1.selectExpr("split(val, ',') as values", "split(val, ',')[4] as species")
+    features_array = raw2.selectExpr("""array(CAST(values[0] AS FLOAT), CAST(values[1] AS FLOAT), CAST(values[2] AS FLOAT), CAST(values[3] AS FLOAT)) as arr""", "species")
+    tovec_udf = funcs.udf(lambda r : Vectors.dense(r),VectorUDT())
+    data_stream = features_array.withColumn("features", tovec_udf("arr"))
+    return data_stream
+
+
+def show30(streaming_df):
+    # show a stream for 30 seconds and close query
+   q = streaming_df.writeStream.outputMode("append").format("console").start()
+   ##    q = streaming_df.writeStream.outputMode("complete").format("console").start()
+   time.sleep(30)
+   q.stop()
+
+
+def predict_on_stream(spark):
+    iris = load_iris(spark)
+    data = create_features_labels(iris)
+    model = train_model(data)
+    stream = get_feature_stream(spark)
+    predictions = model.transform(stream)
+    show30(predictions)
+    #show30(stream)
+
+
+def quiet_logs(spark):
+  logger = spark.sparkContext._jvm.org.apache.log4j
+  logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
+  logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
+
+
+
+def main():
+    spark = pyspark.sql.SparkSession.builder.appName('iris_streaming').getOrCreate()
+    quiet_logs(spark)
+    predict_on_stream(spark)
+
+
+if __name__ == "__main__":
+    main()
